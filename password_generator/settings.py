0e9ea8b9d064c0843c8b3511d367bd1067207eb4
ArslanKhalid <arslan.khalid@purelogics.net>
2021-11-02 20:30:22 +0500

    added req

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -78,9 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
